aa29a54524bf2f291c15e91bc3cb7e05a8067c2f
natalia-hnzn <natalia.hnzn@gmail.com>
2024-03-19 20:20:42 -0300

    feat: estrutura inicial front

---
+++ src/pages/Signup.tsx
@@ -0,0 +1,12 @@
+// import SignupForm from "../features/authentication/SignupForm";
+// import Heading from "../ui/Heading";
+
+function NewUsers() {
+  return (
+    <div>
+      <h1>Cadastro</h1>
+    </div>
+  );
+}
+
+export default NewUsers;
